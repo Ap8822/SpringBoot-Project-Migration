b0200e2771ae50b98fae9bf4dc362d118d8be19d
Ap8822 <alpeshyogi8822@gmail.com>
2026-03-17 18:31:23 -0400

    Use Testcontainer

--- src/test/java/com/ap/microservices/product/ApplicationTests.java
+++ src/test/java/com/ap/microservices/product/ApplicationTests.java
@@ -3,13 +3,17 @@ package com.ap.microservices.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    @Test
+	void shouldCreateProduct() {
+
 	}
 
 }
